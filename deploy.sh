73e3d95f20bf80e1f6dcded75d5d98d80a90c150
Mikhail <misha.ishkov@gmail.com>
2023-04-09 23:00:37 +0500

    123

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,8 @@ cd $name/$path
 docker build . -t $name 
 docker tag $name localhost:5000/$name
 docker push localhost:5000/$name
-docker service create --name $name -p $port:$port_cont --with-registry-auth --replicas $replicas localhost:5000/$name
+docker service create --name $name -p $port:$port_cont --with-registry-auth --replicas $replicas 192.168.0.102:5000/$name
 #docker volume create $name
 #docker run --rm -p $port:$port_cont -d --name $name $name
 
 #config nginx
-
